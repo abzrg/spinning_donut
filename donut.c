8c287b1794ac234a6e18fdba8987199831b84ef4
Ali Bozorgzadeh <aliiiib95@gmail.com>
2022-09-04 23:58:10 +0430

    One declaration/definition per line

--- donut.c
+++ donut.c
@@ -10,7 +10,11 @@ int k;
 
 int main()
 {
-    float A = 0, B = 0, i, j, z[1760];
+    float A = 0;
+    float B = 0;
+    float i;
+    float j;
+    float z[1760];
     char b[1760];
     printf("\x1b[2J");
     for (;;)
@@ -20,13 +24,20 @@ int main()
         for (j = 0; 6.28 > j; j += 0.07)
             for (i = 0; 6.28 > i; i += 0.02)
             {
-                float c = sin(i), d = cos(j), e = sin(A), f = sin(j),
-                      g = cos(A), h = d + 2, D = 1 / (c * h * e + f * g + 5),
-                      l = cos(i), m = cos(B), n = sin(B),
-                      t = c * h * g - f * e;
-                int x   = 40 + 30 * D * (l * h * m - t * n),
-                    y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y,
-                    N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                float c = sin(i);
+                float d = cos(j);
+                float e = sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x   = 40 + 30 * D * (l * h * m - t * n);
+                int y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
                 if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o])
                 {
                     z[o] = D;
