c702281a46b07e38f0807e575c39067f2e32c109
Ali Bozorgzadeh <aliiiib95@gmail.com>
2022-09-06 15:06:35 +0430

    Extract constants (replace magic literals)

--- donut.c
+++ donut.c
@@ -10,16 +10,24 @@ int main()
 {
     float A = 0;
     float B = 0;
-    float z[1760];
-    char b[1760];
+    const size_t kBuffer_size = 1760;
+    float z[kBuffer_size];
+    char b[kBuffer_size];
+    const float kJ_step = 0.07;
+    const float kI_step = 0.02;
+    const int kWidth = 80;
+    const int kHeight = 22;
+    const char *kShade_chars = ".,-~:;=!*#$@";
+    const char kNew_line = '\n';
+    const float kTwo_pi = 2 * M_PI;
     printf("\x1b[2J");
     for (;;)
     {
-        memset(b, 32, 1760);
-        memset(z, 0, 7040);
-        for (float j = 0; 6.28 > j; j += 0.07)
+        memset(b, ' ', kBuffer_size);
+        memset(z, 0, kBuffer_size * sizeof(float));
+        for (float j = 0; kTwo_pi > j; j += kJ_step)
         {
-            for (float i = 0; 6.28 > i; i += 0.02)
+            for (float i = 0; kTwo_pi > i; i += kI_step)
             {
                 float c = sin(i);
                 float d = cos(j);
@@ -32,21 +40,19 @@ int main()
                 float m = cos(B);
                 float n = sin(B);
                 float t = c * h * g - f * e;
-                int x   = 40 + 30 * D * (l * h * m - t * n);
-                int y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y;
+                int x   = (float)(kWidth / 2) + 30 * D * (l * h * m - t * n);
+                int y = (float)(kHeight / 2 + 1) + 15 * D * (l * h * n + t * m), o = x + kWidth * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o])
+                if (kHeight > y && y > 0 && x > 0 && kWidth > x && D > z[o])
                 {
                     z[o] = D;
-                    ;
-                    ;
-                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                    b[o] = kShade_chars[N > 0 ? N : 0];
                 }
             }
         }
         printf("\x1b[H");
-        for (int k = 0; 1761 > k; k++)
-            putchar(k % 80 ? b[k] : 10);
+        for (size_t k = 0; kBuffer_size >= k; k++)
+            putchar(k % kWidth ? b[k] : kNew_line);
         A += 0.04;
         B += 0.02;
     }
