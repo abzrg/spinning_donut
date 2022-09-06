31f8937ec282125df3db5f97648c51bd684c585c
Ali Bozorgzadeh <aliiiib95@gmail.com>
2022-09-06 16:56:25 +0430

    change order of comparison

--- donut.c
+++ donut.c
@@ -25,9 +25,9 @@ int main()
     {
         memset(b, ' ', kBuffer_size);
         memset(z, 0, kBuffer_size * sizeof(float));
-        for (float j = 0; kTwo_pi > j; j += kJ_step)
+        for (float j = 0; j < kTwo_pi; j += kJ_step)
         {
-            for (float i = 0; kTwo_pi > i; i += kI_step)
+            for (float i = 0; i < kTwo_pi; i += kI_step)
             {
                 float c = sin(i);
                 float d = cos(j);
@@ -43,7 +43,7 @@ int main()
                 int x   = (float)(kWidth / 2) + 30 * D * (l * h * m - t * n);
                 int y = (float)(kHeight / 2 + 1) + 15 * D * (l * h * n + t * m), o = x + kWidth * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if (kHeight > y && y > 0 && x > 0 && kWidth > x && D > z[o])
+                if (0 < y && y < kHeight && 0 < x && x < kWidth && D > z[o])
                 {
                     z[o] = D;
                     b[o] = kShade_chars[N > 0 ? N : 0];
@@ -51,7 +51,7 @@ int main()
             }
         }
         printf("\x1b[H");
-        for (size_t k = 0; kBuffer_size >= k; k++)
+        for (size_t k = 0; k <= kBuffer_size; k++)
             putchar(k % kWidth ? b[k] : kNew_line);
         A += 0.04;
         B += 0.02;
